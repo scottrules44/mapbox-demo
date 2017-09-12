3db5912d4b6f7f867d896fe3d31bfe22dce61f8f
scottrules44 <scottrules44@gmail.com>
2017-09-12 11:15:57 -0500

    Bug fixes
    Bug fixes

--- main.lua
+++ main.lua
@@ -1,18 +1,18 @@
 local mapbox = require "plugin.mapbox"
 local json = require "json"
-mapbox.init("pk.eyJ1Ijoic2NvdHRydWxlczQ0IiwiYSI6ImNqNmZqOWprdzBoZDEyd2w5ZTkyZ3Fhc24ifQ.0s3OCQ5XczkeqNvW9Nr9jg")
+mapbox.init("pk.XXXXXXXXXXX")--replace with you access token
 
 local bg = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
 bg:setFillColor( 0, .5, 0)
 local title = display.newText( "Mapbox Plugin", display.contentCenterX, 40, native.systemFontBold, 20 )
 
-local mapview
-local function showInterface()
-
-end
 
 mapbox.newView({x= display.contentCenterX, y = display.contentCenterY, width = 200, height = 200, listener =    function (e)
-    
+        if (e.type == "obj") then
+            local mapview =e.obj
+               mapview:setZoom(0)
+        end
+            
     end
 })
 
